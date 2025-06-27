460683449eed74d831f970f5d5f28b84a6a73682
Labžentis Eimantas <eimantas.labzentis@mif.stud.vu.lt>
2025-06-27 19:32:10 +0300

    1 -project setup

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { signup,login, logout } from '../controllers/auth.controller.js';
+
+dotenv.config();
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+
+export default router;
---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import authRoutes from './routes/auth.routes.js';
+import connectMongoDB from './db/connectMongoDB.js';
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    connectMongoDB();
+})
