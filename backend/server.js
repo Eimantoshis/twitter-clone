f5637cc20f63c47b877862d2cde7b50c9310987d
Labžentis Eimantas <eimantas.labzentis@mif.stud.vu.lt>
2025-06-28 16:15:51 +0300

    1- auth controllers added

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
 import express from 'express';
 import authRoutes from './routes/auth.routes.js';
 import connectMongoDB from './db/connectMongoDB.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.use(express.json()); // to parse req.body
+app.use(express.urlencoded({ extended: true })); // to parse urlencoded data
 
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
